dd9be111845ced253cb7c2edcea933a7f013b580
Drew Olbrich <drew.olbrich@gmail.com>
2018-12-18 07:11:39 -0800

    Initial Commit

---
+++ SeguePerformer/SeguePerformer.h
@@ -0,0 +1,19 @@
+//
+//  SeguePerformer.h
+//  SeguePerformer
+//
+//  Created by Drew Olbrich on 12/18/18.
+//  Copyright © 2018 Drew Olbrich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SeguePerformer.
+FOUNDATION_EXPORT double SeguePerformerVersionNumber;
+
+//! Project version string for SeguePerformer.
+FOUNDATION_EXPORT const unsigned char SeguePerformerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SeguePerformer/PublicHeader.h>
+
+
